78d1c90d6eb0c3e4e06e841e71027fbe88f58a49
Dhruv Nair <dnair3@cs.binghamton.edu>
2019-02-21 18:54:44 -0500

    Vector.cpp edited

--- Vector.cpp
+++ Vector.cpp
@@ -8,18 +8,44 @@ Vector::Vector(){
 }
 
 unsigned Vector::size(){
-	this->p
 	return this->size;	
 }
 	
 
-void Vector::insert(int index, Planet *p){		
+void Vector::insert(int index, Planet *p){			
 	if(p.size() < index)	
-	Planet ** temp = new Planet*[index+1];
-	temp[index] = p;
+		Planet ** p = new Planet*[size+1];
+		Planet *temp = new Planet(index+1);
+		p[this->index] = temp; 
+	else{
+		Planet **p = new Planet[size++];			
+	}	
 }
 
- 
+Planet* Vector::read(int index){
+	if(size < index){
+		return NULL;
+	}
+	else{
+		Planet *temp = this->p[index];
+		return temp;
+	}
+}
+
+bool Vector::remove(){
+	bool flag = TRUE;
+	for(int i=0; i<this->size; i++){
+		if(i == index){
+			delete p[i];
+			p[i] = NULL;
+			this->size--;
+		}
+	}
+	if(flag){ return flag;}
+	return flag;
+}
+
+
 	
 	
 	
